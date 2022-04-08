26928701bfb95b03afacb351bd7a73b7f0dbdd93
unknown <nikitamishra123a@gmail.com>
2022-04-08 15:09:39 +0530

    new change

--- conditions.tf
+++ conditions.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "ec2" {
 ami = "ami-0e472ba40eb589f49"
 instance_type = var.tag_env == "Prod" ? var.instance_type_small : var.instance_type_micro
 tags = {
-    Name = "Demo_condition_machine"
+    Name = "Demo_condition_machine_v2"
 }
 
 }
